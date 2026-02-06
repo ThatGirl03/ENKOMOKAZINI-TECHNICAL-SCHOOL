b6571dd9556bf722aa777648e6f11a573d582f3e
ThatGirl03 <143192466+ThatGirl03@users.noreply.github.com>
2026-02-06 20:32:46 +0200

    Refactor generateSitemap to use static values
    Updated the generateSitemap function to include a fixed URL and last modification date.

--- src/utils/sitemap.ts
+++ src/utils/sitemap.ts
@@ -1,25 +1,9 @@
-// src/utils/sitemap.ts
-export function generateSitemap(): string {
-  const baseUrl = 'https://enkomokazinitech.co.za';
-  const today = new Date().toISOString().split('T')[0];
-  
-  // For single-page app, only list the homepage
-  const pages = [
-    { 
-      loc: '/', 
-      lastmod: today, 
-      priority: '1.0' 
-    }
-  ];
-
-  return `<?xml version="1.0" encoding="UTF-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${pages.map(page => `
   <url>
-    <loc>${baseUrl}${page.loc}</loc>
-    <lastmod>${page.lastmod}</lastmod>
+    <loc>https://enkomokazinitech.co.za/</loc>
+    <lastmod>2024-02-06</lastmod>
     <changefreq>daily</changefreq>
-    <priority>${page.priority}</priority>
-  </url>`).join('')}
-</urlset>`;
-}
+    <priority>1.0</priority>
+  </url>
+</urlset>
